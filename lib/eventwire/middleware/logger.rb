be1995a2b1ceb43fea3223f0de064824df41dd63
Yannick Schutz <yannick.schutz@gmail.com>
2013-02-26 21:29:30 +0100

    Change logger to debug

--- lib/eventwire/middleware/logger.rb
+++ lib/eventwire/middleware/logger.rb
@@ -5,28 +5,28 @@ module Eventwire
         super(app)
         @config = config
       end
-      
+
       def subscribe(event_name, handler_id, &handler)
         @app.subscribe event_name, handler_id do |data|
           begin
-            logger.info "Starting to process `#{event_name}` with handler `#{handler_id}` and data `#{data.inspect}`"
+            logger.debug "Starting to process `#{event_name}` with handler `#{handler_id}` and data `#{data.inspect}`"
             handler.call data
           ensure
-            logger.info "End processing `#{event_name}`"
+            logger.debug "End processing `#{event_name}`"
           end
         end
       end
-      
+
       def publish(event_name, event_data = nil)
         @app.publish event_name, event_data
-        logger.info "Event published `#{event_name}` with data `#{event_data.inspect}`"
+        logger.debug "Event published `#{event_name}` with data `#{event_data.inspect}`"
       end
-     
+
       private
 
       def logger
         @config.logger
-      end   
+      end
     end
   end
 end
